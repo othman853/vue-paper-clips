3b26060e1fc45be835bfb25acbd92de3234b7210
othman853 <anuar.yasser@gmail.com>
2017-10-17 11:58:41 -0200

    HTML as bundle result

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const {join} = require('path')
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const CODE_PATH = join(__dirname, 'src')
 
 module.exports = {
-  entry: join(__dirname, 'src', 'index.js'),
+  entry: join(CODE_PATH, 'index.js'),
   output: {
     path: join(__dirname, 'build'),
     filename: 'dist.js'
@@ -12,7 +15,12 @@ module.exports = {
       {test: /\.vue$/, loader: 'vue-loader'},
       {test: /\.js/, loader: 'babel-loader'}
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: join(CODE_PATH, 'index.html')}
+    ])
+  ]
 }
 
 
