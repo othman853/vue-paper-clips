e08d3a9dbfb037566b71ff56f8553788054b22cd
othman853 <anuar.yasser@gmail.com>
2017-10-21 20:44:10 -0200

    Less white box testing

--- test/unit/store/getters.test.js
+++ test/unit/store/getters.test.js
@@ -1,43 +1,36 @@
+const state = require('store/state')
 const getters = require('store/getters')
 
 describe('unit -> store.getters', () => {
-  it('.clipCount retrieves correct value from statue object', () => {
-    const state = {clip: {count: 5}}
-    expect(getters.clipCount(state)).toBe(5)
+  it('.clipCount retrieves correct value from state object', () => {
+    expect(getters.clipCount(state)).toBe(0)
   })
 
-  it('.clipStock retrieves correct value from statue object', () => {
-    const state = {clip: {stock: 5}}
-    expect(getters.clipStock(state)).toBe(5)
+  it('.clipStock retrieves correct value from state object', () => {
+    expect(getters.clipStock(state)).toBe(0)
   })
 
   it('.clipPrice retrieves correct value from state object', () => {
-    const state = {clip: {price: 5}}
-    expect(getters.clipPrice(state)).toBe(5)
+    expect(getters.clipPrice(state)).toBe(0.25)
   })
 
   it('.wireStock retrieves correct value from state object', () => {
-    const state = {wire: {stock: 5}}
-    expect(getters.wireStock(state)).toBe(5)
+    expect(getters.wireStock(state)).toBe(1000)
   })
 
   it('.funds retrieves correct value from state object', () => {
-    const state = {stats: {funds: 5}}
-    expect(getters.funds(state)).toBe(5)
+    expect(getters.funds(state)).toBe(0)
   })
 
   it('.demand retrieves correct value from state object', () => {
-    const state = {stats: {demand: 5}}
-    expect(getters.demand(state)).toBe(5)
+    expect(getters.demand(state)).toBe(0.3)
   })
 
   it('.wireBasePrice retrieves correct value from state object', () => {
-    const state = {wire: {basePrice: 5}}
-    expect(getters.wireBasePrice(state)).toBe(5)
+    expect(getters.wireBasePrice(state)).toBe(14)
   })
 
   it('.wireBatchSize retrieves correct value from state object', () => {
-    const state = {wire: {batchSize: 5}}
-    expect(getters.wireBatchSize(state)).toBe(5)
+    expect(getters.wireBatchSize(state)).toBe(1000)
   })
 })
